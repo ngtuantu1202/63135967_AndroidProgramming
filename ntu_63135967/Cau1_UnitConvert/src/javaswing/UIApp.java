8e2c365514aaf09f1999fff7a4a1192c4b8f284b
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2024-03-24 11:13:41 +0700

    Bai1_finish

--- ntu_63135967/Cau1_UnitConvert/src/javaswing/UIApp.java
+++ ntu_63135967/Cau1_UnitConvert/src/javaswing/UIApp.java
@@ -32,6 +32,7 @@ public class UIApp extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 621, 535);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 191, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
@@ -110,7 +111,6 @@ public class UIApp extends JFrame {
 
 	    double ketQua= 0;
 	    //Chuyển dổi
-	   
 	    switch (donViNhap) {
         case "Kilometer":
             if (donViXuat.equals("Meter"))
