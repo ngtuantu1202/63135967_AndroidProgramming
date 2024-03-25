7ed5494d9422c6aa261af675a21f2f1722af78ea
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2024-03-25 22:55:28 +0700

    Listener_Lop

--- ntu_63135967/Cau3_IntroductYourself/app/src/main/java/ntu_63135967/introduction/MainActivity.java
+++ ntu_63135967/Cau3_IntroductYourself/app/src/main/java/ntu_63135967/introduction/MainActivity.java
@@ -23,14 +23,19 @@ public class MainActivity extends AppCompatActivity {
         String ten = "Nguyễn Tuấn Tú";
         Toast.makeText(this, ten, Toast.LENGTH_SHORT).show();
     }
-    public void XuLyTLop(View view)
+    public void XuLyLop(View view)
     {
         String lop = "Lớp: CNTT-2";
         Toast.makeText(this, lop, Toast.LENGTH_SHORT).show();
     }
-    public void XuLyTMSSV(View view)
+    public void XuLyMSSV(View view)
     {
         String mssv = "63135967";
         Toast.makeText(this, mssv, Toast.LENGTH_SHORT).show();
+    }
+    public void XuLyEmail(View view)
+    {
+        String email = "tu.nt.63cntt@ntu.edu.vn";
+        Toast.makeText(this, email, Toast.LENGTH_SHORT).show();
     }
 }
