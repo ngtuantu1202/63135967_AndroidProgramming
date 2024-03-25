92f30442bc987604b4e528aab2f0159005c0218c
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2024-03-25 22:41:30 +0700

    Listener_Ten

--- ntu_63135967/Cau3_IntroductYourself/app/src/main/java/ntu_63135967/introduction/MainActivity.java
+++ ntu_63135967/Cau3_IntroductYourself/app/src/main/java/ntu_63135967/introduction/MainActivity.java
@@ -1,6 +1,8 @@
 package ntu_63135967.introduction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    public void XuLyTen(View view)
+    {
+        String ten = "Nguyễn Tuấn Tú";
+        Toast.makeText(this, ten, Toast.LENGTH_SHORT).show();
     }
 }
