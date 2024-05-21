f38282d6e74df58ee2a39978c3245e9b3bd722d1
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2024-05-21 20:51:25 +0700

    fix Fragment

--- TestThiCK/app/src/main/java/ntu/ngtuantu/test/MainActivity.java
+++ TestThiCK/app/src/main/java/ntu/ngtuantu/test/MainActivity.java
@@ -1,24 +1,49 @@
 package ntu.ngtuantu.test;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import ntu.ngtuantu.test.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        BottomNavigationView navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_container, new ProfileFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_profile);
+        //Lang nghe su kien
+        navigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                int itemId = item.getItemId();
+                if (itemId == R.id.nav_profile) {
+                    selectedFragment = new ProfileFragment();
+                }else if (itemId == R.id.nav_cau1) {
+                    selectedFragment = new Cau1Fragment();
+                } else if (itemId == R.id.nav_cau2) {
+                    selectedFragment = new Cau2Fragment();
+                } else if (itemId == R.id.nav_cau3) {
+                    selectedFragment = new Cau3Fragment();
+                }  else if (itemId == R.id.nav_cau4) {
+                    selectedFragment = new Cau4Fragment();
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.body_container, selectedFragment).commit();
+
+                return true;
+            }
         });
     }
 }
