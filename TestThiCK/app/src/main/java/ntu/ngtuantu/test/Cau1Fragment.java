4dade11324c90bc4e68f79c7e2c758067357250b
ngtuantu1202 <125531012+ngtuantu1202@users.noreply.github.com>
2024-06-02 22:28:31 +0700

    Cau 1

--- TestThiCK/app/src/main/java/ntu/ngtuantu/test/Cau1Fragment.java
+++ TestThiCK/app/src/main/java/ntu/ngtuantu/test/Cau1Fragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,8 +17,10 @@ import android.widget.Toast;
 public class Cau1Fragment extends Fragment {
 
     //1.Khai bao
-    EditText editNhap, editXuat;
-    Button buttonDoi;
+    EditText editNhap;
+    Spinner spinnerNhap;
+    Spinner spinnerXuat;
+    TextView txtKQ;
 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -54,27 +57,77 @@ public class Cau1Fragment extends Fragment {
         //2. Tra ve view
         View viewCau1 = inflater.inflate(R.layout.fragment_cau1, container, false);
         //3. Tim dieu khien trong view
-        editNhap =  viewCau1.findViewById(R.id.edtNhap);
-        editXuat = viewCau1.findViewById(R.id.edtXuat);
-        buttonDoi = viewCau1.findViewById(R.id.btnDoi);
+        editNhap = viewCau1.findViewById(R.id.edtNhap);
+        spinnerNhap = viewCau1.findViewById(R.id.spnNhap);
+        spinnerXuat = viewCau1.findViewById(R.id.spnXuat);
+        txtKQ = viewCau1.findViewById(R.id.edtXuat);
+        Button buttonDoi = viewCau1.findViewById(R.id.btnDoi);
         //4.Lang nghe su kien
         buttonDoi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String Nhap = editNhap.getText().toString();
-                String Xuat = editXuat.getText().toString();
-                //
-                if(!Nhap.isEmpty())
-                {
-                    Toast.makeText(viewCau1.getContext(), "m->km", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
-                    Toast.makeText(viewCau1.getContext(), "km->m", Toast.LENGTH_SHORT).show();
-                }
+                XuLyDoi(v);
             }
         });
-
         return viewCau1;
+    }
+    public void XuLyDoi(View view) {
+        // Lấy dữ liệu
+        double giaTriNhap = Double.parseDouble(editNhap.getText().toString());
+        String donViNhap = spinnerNhap.getSelectedItem().toString();
+        String donViXuat = spinnerXuat.getSelectedItem().toString();
+
+        double ketQua = chuyenDoi(donViNhap, donViXuat, giaTriNhap);
+
+        // Hiển thị kết quả
+        txtKQ.setText(String.valueOf(ketQua));
+    }
+
+    private double chuyenDoi(String donViNhap, String donViXuat, double giaTriNhap) {
+        double ketQua = 0;
+
+        switch (donViNhap) {
+            case "km":
+                if (donViXuat.equals("m"))
+                    ketQua = giaTriNhap * 1000;
+                else if (donViXuat.equals("cm"))
+                    ketQua = giaTriNhap * 100000;
+                else if (donViXuat.equals("mm"))
+                    ketQua = giaTriNhap * 1000000;
+                else
+                    ketQua = giaTriNhap;
+                break;
+            case "m":
+                if (donViXuat.equals("km"))
+                    ketQua = giaTriNhap / 1000;
+                else if (donViXuat.equals("cm"))
+                    ketQua = giaTriNhap * 100;
+                else if (donViXuat.equals("mm"))
+                    ketQua = giaTriNhap * 1000;
+                else
+                    ketQua = giaTriNhap;
+                break;
+            case "cm":
+                if (donViXuat.equals("km"))
+                    ketQua = giaTriNhap / 100000;
+                else if (donViXuat.equals("m"))
+                    ketQua = giaTriNhap / 100;
+                else if (donViXuat.equals("mm"))
+                    ketQua = giaTriNhap * 10;
+                else
+                    ketQua = giaTriNhap;
+                break;
+            case "mm":
+                if (donViXuat.equals("km"))
+                    ketQua = giaTriNhap / 1000000;
+                else if (donViXuat.equals("m"))
+                    ketQua = giaTriNhap / 1000;
+                else if (donViXuat.equals("cm"))
+                    ketQua = giaTriNhap / 10;
+                else
+                    ketQua = giaTriNhap;
+                break;
+        }
+        return ketQua;
     }
 }
